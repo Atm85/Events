1aa446988f26152f760a8989d6107f85eace9b0d
Stephen <stevecoulter99@gmail.com>
2019-05-30 12:10:21 -0500

    mysqli is required

--- src/atom/eventManager/Main.php
+++ src/atom/eventManager/Main.php
@@ -25,22 +25,19 @@ class Main extends PluginBase implements Listener {
     public function onEnable(): void {
         self::$instance = $this;
         $cre = $this->getConfig()->getNested("database.mysql");
-        if (in_array("mysqli", get_loaded_extensions())) {
-            if ($cre['host'] != "" &&
-                $cre['username'] != "" &&
-                $cre['schema'] != ""
-            ) {
-                Database::connect();
-                $this->getServer()->getPluginManager()->registerEvents($this, $this);
-                GUI::register("event_creator", Form::event_creator());
-                GUI::register("admin", Form::admin());
-                GUI::register("success", Form::success());
-            } else {
-                $this->getServer()->getPluginManager()->disablePlugin($this);
-            }
-        } else{
+        if ($cre['host'] != "" &&
+            $cre['username'] != "" &&
+            $cre['schema'] != ""
+        ) {
+            Database::connect();
+            $this->getServer()->getPluginManager()->registerEvents($this, $this);
+            GUI::register("event_creator", Form::event_creator());
+            GUI::register("admin", Form::admin());
+            GUI::register("success", Form::success());
+        } else {
             $this->getServer()->getPluginManager()->disablePlugin($this);
         }
+
     }
 
     public function onDisable(): void {
